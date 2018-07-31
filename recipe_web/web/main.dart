b5875858c086bae5fc66ad4c92aaa9b990b3f204
Philip Polasek <ppolasek@users.noreply.github.com>
2018-07-31 07:55:07 -0600

    trying to fix dependency injection

--- recipe_web/web/main.dart
+++ recipe_web/web/main.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2017, ppolasek. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:logging/logging.dart';
@@ -8,27 +10,36 @@ import 'package:http/browser_client.dart';
 
 // ignore: uri_has_not_been_generated
 import 'package:recipe_web/recipes3_app_component.template.dart' as recipes3;
+import 'package:recipe_web/src/common/model.dart';
 
 // ignore: uri_has_not_been_generated
 import 'main.template.dart' as self;
 
 @GenerateInjector([
   routerProvidersHash, // You can use routerProviders in production
-  ClassProvider(BrowserClient),
+  //ClassProvider(BrowserClient),
+  //Provider(BrowserClient, useFactory: () => BrowserClient(), deps: []),
+  const ClassProvider<BrowserClient>(BrowserClient),
+  const FactoryProvider<Recipes3AppConfig>(Recipes3AppConfig, getRecipes3AppConfig),
 ])
 final InjectorFactory injector = self.injector$Injector;
 
-void main() {
+Recipes3AppConfig _recipes3AppConfig;
+Recipes3AppConfig getRecipes3AppConfig() => _recipes3AppConfig;
+
+Future<Null> main() async {
   print('main.main()');
 
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((LogRecord rec) {
     print('${rec.time} ${rec.level.name} ${rec.loggerName} ${rec.message}');
   });
-
+  
 //  const serverUrl = 'http://localhost:9191';
 //  const contextPath = '/Recipes3';
 //  const servletPath = '/services';
+  
+  _recipes3AppConfig = new Recipes3AppConfig('http://localhost:8191', '/Recipes3', '/services');
   
   runApp(recipes3.Recipes3AppComponentNgFactory, createInjector: injector);
 //  bootstrap(Recipes3AppComponent, [
